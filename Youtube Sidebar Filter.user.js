dcb8243555185806e268cf50019d3d72549c2914
Toni Räsänen <rasanen.toni@gmail.com>
2017-02-17 18:57:06 +0200

    Rewrote code so that it uses a listener instead of injecting the script into the HTML Relevant elements in the subscriptions list are now cached

--- Youtube Sidebar Filter.user.js
+++ Youtube Sidebar Filter.user.js
@@ -1,78 +1,93 @@
 // ==UserScript==
 // @name         Youtube Sidebar Filter
 // @namespace    
-// @version      0.1
+// @version      0.2
 // @description  Adds filter field to YouTube's sidebar for Subscriptions
 // @author       zhal9k
-// @match        *://*.youtube.com/*
+// @match        http*://*.youtube.com/*
 // @grant        none
+// @run-at document-idle
 // ==/UserScript==
 
 (function() {
     "use strict";
 
+    var filterBar;
+    var elements = [ ];
+
     var subsList = document.getElementById("guide-channels");
+
     if (subsList !== null)
     {
+        addFilterBar();
+    }
+
+    function addFilterBar()
+    {
         var subsParent = subsList.parentElement;
-        var inputFilter = document.createElement("input");
-        inputFilter.setAttribute("id", "zhal-subs-filter");
-        inputFilter.setAttribute("class", "search-term");
-        inputFilter.setAttribute("style", "height: 20px;");
-        inputFilter.setAttribute("placeholder", "Filter...");
-        inputFilter.setAttribute("onkeyup", "filterElements()");
-        subsParent.insertBefore(inputFilter, subsList);
+        filterBar = document.createElement("input");
+        filterBar.setAttribute("id", "zhal-subs-filter");
+        filterBar.setAttribute("class", "search-term");
+        filterBar.setAttribute("style", "height: 20px;");
+        filterBar.setAttribute("placeholder", "Filter...");
+        filterBar.addEventListener("keyup", function() { filterElements(); }, false);
+        subsParent.insertBefore(filterBar, subsList);
 
-        var filterScript = document.createElement("script");
-        filterScript.type = 'text/javascript';
-        var code = "" +
-            "function filterElements()" +
-			"{" +
-            "    var inputFilter = document.getElementById(\"zhal-subs-filter\");" +
-            "    var filter = inputFilter.value.toUpperCase();" +
-            "    var subsList = document.getElementById(\"guide-channels\");" +
-            "    var subs = subsList.getElementsByClassName(\"display-name\");" +
-            "    var a, i, displayStyle;" +
+        var subNames = subsList.getElementsByClassName("display-name");
 
-            "    for (i = 0; i < subs.length; i++)" +
-			"	 {" +
-            "        a = subs[i].childNodes[1];" +
-            "        if (a.innerHTML.toUpperCase().indexOf(filter) > -1)" +
-			"		 {" +
-            "            getAncestorLI(subs[i]).style.display = \"\";" +
-            "        }" +
-			"		 else" +
-			"		 {" +
-            "            getAncestorLI(subs[i]).style.display = \"none\";" +
-            "        }" +
-            "    }" +
-            "}" +
-            "" +
-            "function getAncestorLI(el)" +
-			"{" +
-            "    var retval = null;" +
-            "    while (el)" +
-			"	 {" +
-            "        if (el.tagName.indexOf(\"LI\") > -1)" +
-			"		 {" +
-            "            retval = el;" +
-            "            break;" +
-            "        }" +
-			"		 else if (el.tagName.indexOf(\"UL\") > -1)" +
-			"		 {" +
-            "            break;" +
-            "        }" +
-            "        el = el.parentElement;" +
-            "    }" +
-            "    return retval;"+
-            "}";
-
-        try {
-            filterScript.appendChild(document.createTextNode(code));
-            document.body.appendChild(filterScript);
-        } catch (e) {
-            filterScript.text = code;
-            document.body.appendChild(filterScript);
+        for (var ii = 0; ii < subNames.length; ii++)
+        {
+            var t = subNames[ii].childNodes[1];
+            elements.push({ name: t.innerHTML.toUpperCase(), element: getAncestorLI(subNames[ii]) });
         }
     }
+
+    function filterElements()
+    {
+        if (filterBar === null)
+        {
+            filterBar = document.getElementById("zhal-subs-filter");
+        }
+
+        var filterBy = filterBar.value.toUpperCase();
+        var temp, ii;
+        for (ii = 0; ii < elements.length; ii++)
+        {
+            temp = elements[ii];
+            if (temp.name.indexOf(filterBy) > -1)
+            {
+                if (temp.element.style.display !== "")
+                {
+                    temp.element.style.display = "";
+                }
+            }
+            else
+            {
+                if (temp.element.style.display !== "none")
+                {
+                    temp.element.style.display = "none";
+                }
+            }
+        }
+    }
+
+    function getAncestorLI(el)
+    {
+        var retval = null;
+        while (el)
+        {
+            if (el.tagName.indexOf("LI") > -1)
+            {
+                retval = el;
+                break;
+            }
+            else if (el.tagName.indexOf("UL") > -1)
+            {
+                break;
+            }
+            el = el.parentElement;
+        }
+        return retval;
+    }
 })();
+
