2ed9177eb7d183c4bf440d426c5f0b4606c8dd47
Toni Räsänen <rasanen.toni@gmail.com>
2017-02-27 17:29:02 +0200

    Added support for Playlists section Filtering is now also done for subtitles (saved playlists have their owner as subtitle)

--- Youtube Sidebar Filter.user.js
+++ Youtube Sidebar Filter.user.js
@@ -1,10 +1,13 @@
 // ==UserScript==
 // @name         Youtube Sidebar Filter
 // @namespace    
-// @version      0.3
-// @description  Adds filter field to YouTube's sidebar for Subscriptions
+// @version      1.0
+// @description  Adds filter bars to YouTube's sidebar for Subscriptions and Playlists
 // @author       zhal9k
 // @match        http*://*.youtube.com/*
+// @exclude      *://www.youtube.com/tv*
+// @exclude      *://www.youtube.com/embed/*
+// @exclude      *://www.youtube.com/live_chat*
 // @grant        none
 // @run-at document-idle
 // ==/UserScript==
@@ -12,24 +15,62 @@
 (function() {
     "use strict";
 
-    var filterBar = null;
-    var elements = [ ];
-    var subsList = document.getElementById("guide-channels");
+    var subsSelectorId = "guide-channels";
+    var subsFilterBarId = "zhal-subs-filter";
+    var playlistsSelector = "#guide-container > div > ul > li:nth-child(2) > div > ul";
+    var playlistsFilterBarId = "zhal-playlists-filter";
+    var guideButtonId = "appbar-guide-button";
 
-    if (subsList !== null)
+    var subsFilterBar = null;
+    var subsElements = [];
+    var subsUL = document.getElementById(subsSelectorId);
+
+    var playlistsFilterBar = null;
+    var playlistElements = [];
+    var playlistsUL = document.querySelector(playlistsSelector);
+
+
+    initSubsFilterBar();
+    initPlaylistsFilterBar();
+
+    function initSubsFilterBar()
     {
-        addFilterBar();
-    }
-    else
-    {
-        var guideButton = document.getElementById("appbar-guide-button");
-        guideButton.addEventListener("click", addFilterBarOnGuideClick, false);
+        if (subsUL !== null)
+        {
+            subsFilterBar = createFilterBar(subsUL, subsFilterBarId, filterSubs);
+            populateElements(subsElements, subsUL);
+        }
+        else
+        {
+            var guideButton = document.getElementById(guideButtonId);
+            if (guideButton !== null)
+            {
+                guideButton.addEventListener("click", addSubsFilterBarOnGuideClick, false);
+            }
+        }
     }
 
-    function addFilterBarOnGuideClick()
+    function initPlaylistsFilterBar()
+    {
+        if (playlistsUL !== null)
+        {
+            playlistsFilterBar = createFilterBar(playlistsUL, playlistsFilterBarId, filterPlaylists);
+            populateElements(playlistElements, playlistsUL);
+        }
+        else
+        {
+            var guideButton = document.getElementById(guideButtonId);
+            if (guideButton !== null)
+            {
+                guideButton.addEventListener("click", addPlaylistsFilterBarOnGuideClick, false);
+            }
+        }
+    }
+
+    function addSubsFilterBarOnGuideClick()
     {
         var timerFunc;
-        if (filterBar === null)
+        if (subsFilterBar === null)
         {
             timerFunc = setInterval(myTimer, 1000);
         }
@@ -39,55 +80,106 @@
         function myTimer()
         {
             count++;
-            subsList = document.getElementById("guide-channels");
-            if (filterBar === null && subsList !== null)
+            subsUL = document.getElementById(subsSelectorId);
+            if (subsFilterBar === null && subsUL !== null)
             {
-                addFilterBar();
-                if (filterBar !== null)
-                {
-                    clearInterval(timerFunc);
-                }
+                subsFilterBar = createFilterBar(subsUL, subsFilterBarId, filterSubs);
+                populateElements(subsElements, subsUL);
             }
-            if (count > 5)
+            if (count > 5 || subsFilterBar !== null)
             {
                 clearInterval(timerFunc);
             }
         }
     }
 
-    function addFilterBar()
+    function addPlaylistsFilterBarOnGuideClick()
     {
-        var subsParent = subsList.parentElement;
-        filterBar = document.createElement("input");
-        filterBar.setAttribute("id", "zhal-subs-filter");
-        filterBar.setAttribute("class", "search-term");
-        filterBar.setAttribute("style", "height: 20px;");
-        filterBar.setAttribute("placeholder", "Filter...");
-        filterBar.addEventListener("keyup", filterElements, false);
-        subsParent.insertBefore(filterBar, subsList);
-
-        var subNames = subsList.getElementsByClassName("display-name");
-
-        for (var ii = 0; ii < subNames.length; ii++)
+        var timerFunc;
+        if (playlistsFilterBar === null)
         {
-            var t = subNames[ii].childNodes[1];
-            elements.push({ name: t.innerHTML.toUpperCase(), element: getAncestorLI(subNames[ii]) });
+            timerFunc = setInterval(myTimer, 1000);
+        }
+
+        var count = 0;
+
+        function myTimer()
+        {
+            count++;
+            playlistsUL = document.querySelector(playlistsSelector);
+            if (playlistsFilterBar === null && playlistsUL !== null)
+            {
+                playlistsFilterBar = createFilterBar(playlistsUL, playlistsFilterBarId, filterPlaylists);
+                populateElements(playlistElements, playlistsUL);
+            }
+            if (count > 5 || playlistsFilterBar !== null)
+            {
+                clearInterval(timerFunc);
+            }
         }
     }
 
-    function filterElements()
+    function createFilterBar(listUL, id, listener)
     {
-        if (filterBar === null)
+        var t = document.getElementById(id);
+        if (t === null)
         {
-            filterBar = document.getElementById("zhal-subs-filter");
+            var parent = listUL.parentElement;
+            var filterBar = document.createElement("input");
+            filterBar.setAttribute("id", id);
+            filterBar.setAttribute("class", "search-term");
+            filterBar.setAttribute("style", "height: 20px");
+            filterBar.setAttribute("placeholder", "Filter...");
+            filterBar.addEventListener("keyup", listener, false);
+            parent.insertBefore(filterBar, listUL);
+            return filterBar;
         }
+        else
+        {
+            console.log(id + " already created");
+            return t;
+        }
+    }
 
-        var filterBy = filterBar.value.toUpperCase();
+    function populateElements(elements, listUL)
+    {
+        var names = listUL.getElementsByClassName("yt-valign-container");
+        for (var ii = 0; ii < names.length; ii++)
+        {
+            var el = getAncestorLI(names[ii]);
+            if (el !== null)
+            {
+                var name = names[ii].innerText.toUpperCase();
+                var subtitle = "";
+                var subtitles = names[ii].getElementsByClassName("guide-item-subtitle");
+                if (subtitles.length > 0)
+                {
+                    subtitle = subtitles[0].innerText;
+                }
+                elements.push({ name: name, subtitle: subtitle, element: getAncestorLI(names[ii]) });
+            }
+        }
+    }
+
+    function filterSubs()
+    {
+        var filterBy = subsFilterBar.value.toUpperCase();
+        filterElements(filterBy, subsElements);
+    }
+
+    function filterPlaylists()
+    {
+        var filterBy = playlistsFilterBar.value.toUpperCase();
+        filterElements(filterBy, playlistElements);
+    }
+
+    function filterElements(filterBy, elements)
+    {
         var temp, ii;
         for (ii = 0; ii < elements.length; ii++)
         {
             temp = elements[ii];
-            if (temp.name.indexOf(filterBy) > -1)
+            if (temp.name.indexOf(filterBy) > -1 || temp.subtitle.indexOf(filterBy) > -1)
             {
                 if (temp.element.style.display !== "list-item")
                 {
